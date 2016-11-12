47b1b410dd1432bb0697f139bfa2cd7e2971d8dd
unicorn <unicorn@eqmud.mooo.com>
2016-11-12 13:26:45 +1300

    add sequence encoder

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 
-sequence=11111111111111111111111111111111111111111111111111111111111111
+# the code from switches in the remote
+code=111111001110
+
+# encode the carrier on/off sequence
+sequence=$(echo -n "0${code}0" | tr '01' '10' | sed -e 's/./10&/g' -e 's/$/11111/')
+
+# repeat 5 times with some leading 1's to give receiver time to warm up,
+# and a trailing 0 just to make sure we turn off the carrier
+sequence=1111${sequence}${sequence}${sequence}${sequence}${sequence}0
 
 # Set 40.685MHz carrier frequency
 echo 40685000 > /sys/devices/platform/garage-door/carrier
